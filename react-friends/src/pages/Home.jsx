a81543cd20db347f955e383b5b1a5cee7a28e2b7
devrachaelsimone <108482417+devrachaelsimone@users.noreply.github.com>
2023-12-05 15:23:27 -0600

    getting response from api using getFriendsThunk
    getting correct array from friends db using getFriendsThunk. need to correctly add it to the new state to be displayed onto the UI. redux state showing empty array

--- react-friends/src/pages/Home.jsx
+++ react-friends/src/pages/Home.jsx
@@ -5,18 +5,38 @@ import { StyledButton } from "../styledcomponents/StyledButton";
 import FriendTable from "../components/FriendTable";
 import { useNavigate } from "react-router-dom";
 import { getFriends, getFriendsThunk } from "../redux/friend/friendsSlice";
+import {
+  TableBody,
+  TableContainer,
+  TableHead,
+  Paper,
+  Button,
+  ButtonGroup,
+} from "@mui/material";
+import styled from "@emotion/styled";
+import { StyledTableCell } from "../styledcomponents/StyledTableCell";
+import { StyledTableRow } from "../styledcomponents/StyledTableRow";
+import { StyledTable } from "../styledcomponents/StyledTable";
 import Loading from "../components/Loading.jsx"
 
 const Home = () => {
   const navigate = useNavigate();
-  const [loading, setLoading] = useState(true); // Add loading state
-  const friends = useSelector((state) => state.friends);
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
+  
+   //define the various friends states
+   const friends = useSelector(state => state.friends); 
+   const error = useSelector((state) => state.friends.error);
+
+
+  //1. grab the data with dispatch
   useEffect(() => {
-    // Fetch the list of friends when the component mounts or whenever needed
-    dispatch(getFriendsThunk, getFriends());
-  }, []);
+    dispatch(getFriendsThunk());
+    console.log(friends);
+  }, [dispatch]); // Include dispatch as a dependency
+
+ 
+
   
   return (
     <>
@@ -31,7 +51,119 @@ const Home = () => {
           }}
         >
           <h2>this is home</h2>
-          <FriendTable/>
+          
+          {/*  */}
+
+
+
+
+
+
+
+
+
+
+
+
+
+          <TableContainer
+      component={Paper}
+      elevation={24}
+      sx={{
+        backgroundColor: "cornflowerblue",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        mx: 0.5,
+        mb: 3,
+      }}
+    >
+
+      <StyledTable aria-label="customized table">
+        <TableHead>
+          <StyledTableRow>
+            <StyledTableCell>Name</StyledTableCell>
+            <StyledTableCell align="center">Email</StyledTableCell>
+            <StyledTableCell align="center">Stream</StyledTableCell>
+            <StyledTableCell align="center">Address</StyledTableCell>
+            <StyledTableCell align="center">Action</StyledTableCell>
+          </StyledTableRow>
+        </TableHead>
+        <TableBody>
+           {friends && friends.length > 0 ? ( 
+            friends.map((friend) => (
+              <StyledTableRow key={friend.id}
+              >
+                <StyledTableCell component="th" scope="row">
+                  {friend.name}
+                </StyledTableCell>
+                <StyledTableCell align="center">{friend.email}</StyledTableCell>
+                <StyledTableCell align="center">
+                  {friend.stream}
+                </StyledTableCell>
+                <StyledTableCell align="center">
+                  {friend.address}
+                </StyledTableCell>
+                <StyledTableCell align="center">
+                  <div>
+                    <ButtonGroup
+                      variant="contained"
+                      aria-label="friend actions"
+                    >
+                      <Button>View</Button>
+                      <Button color="primary" style={{ marginRight: "5px" }}>
+                        {" "}
+                        Edit{" "}
+                      </Button>
+                      <Button 
+                      color="secondary" style={{ marginRight: "5px" }}>
+                        Delete
+                      </Button>
+                    </ButtonGroup>
+                  </div>
+                </StyledTableCell>
+              </StyledTableRow>
+            ))
+          ) : (
+            <StyledTableRow>
+              <StyledTableCell component="th" scope="row" colSpan={5}>
+                <Loading/>
+              </StyledTableCell>
+            </StyledTableRow>
+          )}
+        </TableBody>
+      </StyledTable>
+    </TableContainer>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          {/*  */}
           <div>
             <StyledButton onClick={() => navigate("/addfriend")}>
               Add friend
--- react-friends/src/redux/friend/friendsSlice.js
+++ react-friends/src/redux/friend/friendsSlice.js
@@ -3,7 +3,7 @@ import axios from "axios";
 /* =========================== THUNKS ============================= */
 export const getFriendsThunk = createAsyncThunk("friends", async () => {
   try {
-    const response = axios.get(`${process.env.REACT_APP_API_URL}/friends`);
+    const response = await axios.get(`${process.env.REACT_APP_API_URL}/friends`);
     console.log("API Response:", response.data)
     return response.data;
   } catch (err) {
@@ -11,11 +11,7 @@ export const getFriendsThunk = createAsyncThunk("friends", async () => {
   }
 });
 
-//create thunk
 
-//update/edit thunk
-
-//delete thunk
 
 /* =========================== SLICE ============================= */
 const friendsSlice = createSlice({
@@ -33,25 +29,10 @@ const friendsSlice = createSlice({
       state.friends = action.payload;
     },
   },
-  extraReducers: {
-    //================================================== get actions
-      [getFriendsThunk.pending]: (state) => {
-        state.loading = true;
-        state.error = null;
-      },
-      [getFriendsThunk.fulfilled]: (state, action) => {
-        state.loading = false;
-        state.friends = action.payload;
-      },
-      [getFriendsThunk.rejected]: (state, action) => {
-        state.loading = false;
-        state.error = action.error.message;
-      },
-    //==============================================================
-  },
 });
 
 //export const { getFriends, addFriend, deleteFriend } = friendsSlice.actions;
 export const { addFriend, getFriends } = friendsSlice.actions;
 
 export default friendsSlice.reducer;
+
